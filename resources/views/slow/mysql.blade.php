a2c83a92e3c6824471102b6545721d091244725d
LuminEe <1123462990@qq.com>
2019-03-14 11:35:44 +0800

    update slow mysql

--- resources/views/slow/mysql.blade.php
+++ resources/views/slow/mysql.blade.php
@@ -9,12 +9,14 @@
         @endforeach
         <a class="btn btn-warning pull-right" id="toggle-hide">Show / Hide</a>
         <table class="table table-bordered table-hover">
-            <caption>[ 时间阀值:{{$_sec}}s] {!! \Carbon\Carbon::now()->subDays($_day) !!} - {!! date('Y-m-d H:i:s') !!}</caption>
+            <caption>[ 时间阀值:{{$_sec}}s] {!! \Carbon\Carbon::now()->subDays($_day) !!}
+                - {!! date('Y-m-d H:i:s') !!}</caption>
             @foreach($times as $key=>$v)
                 <tr>
                     <td @if(strstr($sql_s[$key]['date'],' 00:') && strstr($sql_s[$key]['user'],'manage')) class="need-hide" @endif>
                         <span class="label @if($v >= $_sec) bg-red @else bg-gray @endif">{{$v}}s</span>
-                        <span class="label @if(strstr($sql_s[$key]['user'],'manage')) bg-green @else bg-blue @endif">
+                        <span class="label @if(strstr($sql_s[$key]['user'],'manage'))
+                                bg-green @elseif(strstr($sql_s[$key]['user'],'sql_analyze')) bg-orange @else bg-blue @endif">
                         {{$sql_s[$key]['user']}}</span><span> @ {{$sql_s[$key]['host']}}[{{$sql_s[$key]['date']}}]</span>
                         <br>
                         <span>{{$sql_s[$key]['sql']}}</span>
