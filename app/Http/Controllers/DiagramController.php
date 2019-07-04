0d9ef66423acd28a1f5c5aa174506572255e0c69
LuminEe <1123462990@qq.com>
2019-07-04 11:09:48 +0800

    add common

--- app/Http/Controllers/DiagramController.php
+++ app/Http/Controllers/DiagramController.php
@@ -9,13 +9,11 @@ class DiagramController extends Controller
     public function uml(Request $request)
     {
         $_project = $request->get('project', 'core');
-        $projects = ['core' => '在线助教', 'learning' => '百项过'];
+        $projects = ['core' => '在线助教', 'learning' => '百项过', 'common' => '通用'];
         $uml_dir = public_path('asset/diagrams/uml/');
-        $images = ['Modules.jpg'];
+        $images = file_exists($uml_dir . $_project . '/Modules.jpg') ? ['Modules.jpg'] : [];
         foreach (scandir($uml_dir . $_project) as $file) {
-            if (!in_array($file, ['.', '..']) && !strstr($file, 'Modules')) {
-                $images[] = $file;
-            }
+            if (!in_array($file, ['.', '..', '.DS_Store']) && !strstr($file, 'Modules')) $images[] = $file;
         }
         $dir = 'asset/diagrams/uml/' . $_project . '/';
         return view('diagram.uml', compact('projects', '_project', 'images', 'dir'));
