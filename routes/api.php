f3f0d650a7501685bca7353fc625d2947a617397
LuminEe <1123462990@qq.com>
2019-07-18 14:55:04 +0800

    update audit

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::group([], function () {
+    Route::get('audit/get/rules', function () {
+        $rules = include storage_path('/') . 'audit_rule.php';
+        return json_encode($rules);
+    });
+});
---
+++ storage/audit_rule.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'example' => [
+
+        'ignore' => [
+
+            'user_type'
+        ],
+    ],
+];
