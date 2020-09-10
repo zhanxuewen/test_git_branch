ca3e0f3631be8d5bb3f18a12108e0ee0c10e19c8
LuminEe <1123462990@qq.com>
2020-09-10 11:46:23 +0800

    Update system config blade

--- resources/views/system/config.blade.php
+++ resources/views/system/config.blade.php
@@ -14,21 +14,26 @@
             <small>[config effect 24 hours.]</small>
         </h4>
         <div class="col-sm-12">
-            <form action="{{url('system/config')}}" method="POST">
+            <form action="{{url('system/config')}}" class="form-horizontal" method="POST">
                 {{csrf_field()}}
                 <input type="hidden" name="config_type" value="personal">
-                <div class="form-group col-sm-2">
-                    <label for="conn">审计链接</label>
-                    <select name="conn" id="conn" class="form-control">
-                        @foreach($conn_s as $_conn => $label)
-                            <option value="{{$_conn}}" @if($conn == $_conn) selected @endif>{{$label}}</option>
-                        @endforeach
-                    </select>
+                <div class="form-group">
+                    <label for="conn" class="col-sm-2 control-label">审计链接</label>
+                    <div class="col-sm-3">
+                        <select name="conn" id="conn" class="form-control">
+                            @foreach($conn_s as $_conn => $label)
+                                <option value="{{$_conn}}" @if($conn == $_conn) selected @endif>{{$label}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group col-sm-1">
-                    <label for="per-page">分页条数</label>
-                    <input type="text" name="perPage" id="per-page" value="{{$perPage}}" size="5"
-                           style="ime-mode:disabled" onKeyUp="this.value = numberCheck(this.value, 100)"/>
+                <div class="form-group">
+                    <label for="per-page" class="col-sm-2 control-label">分页条数</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control" name="perPage" id="per-page" value="{{$perPage}}"
+                               size="5"
+                               style="ime-mode:disabled" onKeyUp="this.value = numberCheck(this.value, 100)"/>
+                    </div>
                 </div>
                 <div class="col-sm-12">
                     <input class="btn btn-primary" type="submit" value="设置">
